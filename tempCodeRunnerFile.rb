7862aa391ab4b236683095e818c79b28c8bbbf7c
William Sosropartono <wcsosropartono@gmail.com>
2022-06-12 20:53:51 -0700

    Implemented substring function

--- tempCodeRunnerFile.rb
+++ tempCodeRunnerFile.rb
@@ -1,11 +1,7 @@
-        # if element.ord.between?(97, 122)
-        #     if element.ord + key > 122
-        #         (element.ord + key - 26).chr
-        #     end
-        # end
-        # if element.ord.between?(65, 90)
-        #     if element.ord+key > 90
-        #         (element.ord + key-26).chr
-        #     end
-        # end
-        # (element.ord + key).chr
+    # arr_of_strings.reduce(Hash.new(0)) do |accumulator, element|
+    #     if dictionary.include?(element) then
+    #         accumulator[element] += 1
+    #         accumulator
+    #     end
+    
+    # end
